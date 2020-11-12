62aa2b6c6fe01a6387e0c846259fbd16dc2c629d
Thijs Damsma <tdamsma@gmail.com>
2020-11-12 22:24:05 +0100

    fix header

--- components/Header.vue
+++ components/Header.vue
@@ -4,15 +4,17 @@ div
     .container.d-flex.justify-content-between
       b-navbar-brand(href="#")
         img(src="/images/Logo-Yep-300x122.png", alt="yeptrainingen")
-      b-navbar-toggle(target="nav-collapse")
-        b-collapse#nav-collapse(is-nav="")
-          b-navbar-nav
-            b-nav-item(href="#") Yep
-            b-nav-item(href="#") Ons team
-            b-nav-item(href="#") Trainingsaanbod
-            b-nav-item(href="#") Coachingsaanbod
-            b-nav-item(href="#") Voor werkgevers
-            b-nav-item(href="#") Blog
+
+      b-navbar-toggle target="nav-collapse"
+
+      b-collapse#nav-collapse(is-nav="")
+        b-navbar-nav
+          b-nav-item(href="#") Yep
+          b-nav-item(href="#") Ons team
+          b-nav-item(href="#") Trainingsaanbod
+          b-nav-item(href="#") Coachingsaanbod
+          b-nav-item(href="#") Voor werkgevers
+          b-nav-item(href="#") Blog
 </template>
 
 <style>

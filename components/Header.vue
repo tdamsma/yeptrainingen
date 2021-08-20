004f21c3c62ac6cc5f652beb4933e767546b68ac
Thijs Damsma <tdamsma@gmail.com>
2021-08-20 22:21:20 +0200

    Alle wijzigingen Gijs (#135)
    Co-authored-by: Gijs <gijs@webfacet.nl>

--- components/Header.vue
+++ components/Header.vue
@@ -1,24 +1,20 @@
 <template lang="pug">
 div
-  b-navbar(toggleable="lg", type="light", variant="light")
+  b-navbar(toggleable='lg', type='light', variant='light')
     .container.d-flex.justify-content-between
-      b-navbar-brand(href="#")
-        img.ml-2(
-          src="~/assets/images/Logo-Yep-300x122.png",
-          alt="yeptrainingen"
-        )
+      b-navbar-brand(href='#')
+        img.ml-2(src='~/assets/images/Logo-Yep-300x122.png', alt='yeptrainingen')
 
-      b-navbar-toggle.mr-2(target="nav-collapse")
+      b-navbar-toggle.mr-2(target='nav-collapse')
 
-      b-collapse#nav-collapse(is-nav="")
+      b-collapse#nav-collapse(is-nav='')
         b-navbar-nav.ml-4
-          b-nav-item(to="/") Yep
-          b-nav-item(href="/team") Ons team
-          b-nav-item(href="/trainingsaanbod") Trainingen
-          b-nav-item(href="/coachingsaanbod") Coachingsaanbod
-          b-nav-item(href="/coaching") Coaching
-          b-nav-item(href="#") Voor werkgevers
-          b-nav-item(to="/blog") Blog
+          b-nav-item(to='/') Yep
+          b-nav-item(href='/team') Ons team
+          b-nav-item(href='/trainingsaanbod') Trainingen
+          b-nav-item(href='/coaching') Coaching
+          b-nav-item(href='#') Voor werkgevers
+          b-nav-item(to='/blog') Blog
 </template>
 
 <style lang="scss">

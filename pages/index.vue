4b94cf62f1f37c16dab22272140982704bd750c7
Thijs Damsma <tdamsma@gmail.com>
2021-08-20 22:21:20 +0200

    Alle wijzigingen Gijs (#135)
    Co-authored-by: Gijs <gijs@webfacet.nl>

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
         </p>
       </div>
     </div>
-    <div class="container-fluid container-right">
+    <div class="container-fluid container-left">
       <div class="row">
         <div class="container">
           <div class="div" style="float: left; max-width: 600px; width: 100%">
@@ -31,7 +31,26 @@
       </div>
     </div>
     <div class="jumbotron jumbotron-fluid yep-geel">
-      <div class="container"></div>
+      <div class="container">
+        <h2>De drang om te leren.</h2>
+        <h2>Wij zien het bij zo veel professionals.</h2>
+        <h2>En we weten hoe graag organisaties daar ruimte voor willen maken.</h2>
+        <p>
+          Wij zijn er voor professionals die staan te popelen om hun stem verder te ontdekken en te laten horen. Die een idee hebben over hoe het anders kan en
+          willen weten hoe ze dat binnen hun organisatie waar kunnen maken. Die soms nog aan zichzelf twijfelen en merken dat organisaties complex zijn. Maar
+          die zich razendsnel nieuwe vaardigheden eigen willen maken. Enerzijds om zelf te groeien, anderzijds omdat ze weten dat ze een tastbare bijdrage
+          kunnen leveren aan de organisatie en de samenleving als ze echt ergens voor durven staan.
+        </p>
+      </div>
+    </div>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <div class="row justify-content-around">
+          <div class="col-md-3 border bg-light p-2 text-center"><h2>REVIEWS</h2></div>
+          <div class="col-md-3 border bg-light p-2 text-center"><h2>TOP PROGRAMMA</h2></div>
+          <div class="col-md-3 border bg-light p-2 text-center"><h2>BLOGS</h2>Hoe begeleid ik een hybride overleg?</div>
+        </div>
+      </div>
     </div>
     <div class="container-fluid container-right">
       <div class="row">
@@ -52,32 +71,88 @@
         </div>
       </div>
     </div>
-    <div class="jumbotron jumbotron-fluid yep-geel">
+    <div class="jumbotron jumbotron-fluid" style="background-color:#585656">
       <div class="container">
-        <h2>De drang om te leren.</h2>
-        <h2>Wij zien het bij zo veel professionals.</h2>
-        <h2>En we weten hoe graag organisaties daar ruimte voor willen maken.</h2>
-        <p>
-          Wij zijn er voor professionals die staan te popelen om hun stem verder te ontdekken en te laten horen. Die een idee hebben over hoe het anders kan en
-          willen weten hoe ze dat binnen hun organisatie waar kunnen maken. Die soms nog aan zichzelf twijfelen en merken dat organisaties complex zijn. Maar
-          die zich razendsnel nieuwe vaardigheden eigen willen maken. Enerzijds om zelf te groeien, anderzijds omdat ze weten dat ze een tastbare bijdrage
-          kunnen leveren aan de organisatie en de samenleving als ze echt ergens voor durven staan.
-        </p>
+        <h1 class="text-center text-light">Opdrachtgevers</h1>
+      </div>
+    </div>
+    <div class="container-fluid m-0">
+            <b-img
+              fluid="fluid"
+              blank-color="#777"
+              src="~/assets/images/site-foto-1-of-1-3.jpg"
+              :srcset="require(`~/assets/images/site-foto-1-of-1-3.jpg`).srcSet"
+            ></b-img> 
+    </div>
+    <div class="jumbotron jumbotron-fluid" style="background-color:#585656">
+      <div class="container text-light border-top border-bottom">
+        <h1 class="text-center my-4">Yep is hét trainingsbureau voor gedreven professionals.</h1>
+         <div class="row justify-content-around my-5">
+          <div class="col-md-3 p-2 text-center">
+            <h2>Krachtig</h2>
+            <p>In korte tijd zichtbaar effectief.</p>
+          </div>
+          <div class="col-md-3 p-2 text-center">
+            <h2>Actief</h2>
+            <p>Leren door te experimenteren.</p>
+          </div>
+          <div class="col-md-3 text-center">
+            <h2>Flexibel</h2>
+             <p>Jouw ontwikkelvraag staat centraal.</p>
+          </div>
+        </div>
+        <p class="text-center my-5">Voorbeelden van onze trainingen en case studies vind je hier. Op zoek naar coaching voor jezelf of je medewerkers? Lees hier verder>>.</p>
       </div>
     </div>
     <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1>Yep is hét trainingsbureau voor gedreven professionals.</h1>
-        <h2>Krachtig</h2>
-        <p>In korte tijd zichtbaar effectief.</p>
-        <h2>Actief</h2>
-        <p>Leren door te experimenteren.</p>
-        <h2>Flexibel</h2>
-
-        <p>Jouw ontwikkelvraag staat centraal.</p>
-        <p>Voorbeelden van onze trainingen en case studies vind je hier. Op zoek naar coaching voor jezelf of je medewerkers? Lees hier verder.</p>
+      <div class="container text-center">
+        <h1>Bekijk ons aanbod</h1>
+        <div class="row justify-content-around mt-3">
+          <div class="col-md-4 border bg-light p-3 text-center">
+            <h2>Trainingen</h2>
+            <p>Lees meer >></p>
+          </div>
+          <div class="col-md-4 border bg-light p-3 text-center">
+            <h2>Coaching</h2>
+            <p>Lees meer >></p>
+            </div>
+        </div>
       </div>
     </div>
+    <div class="jumbotron jumbotron-fluid" style="background-color:#585656">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 text-light">
+            <h2>Menu</h2>
+          </div>
+          <div class="col-md-3 text-light">
+            <h2 class="mb-4">Gegevens</h2>
+            <p>Yep Trainingen<br>
+               <br>
+               <br><br>
+               KvK: 58989463<br>
+               BTW: NL001795751B66<br>
+               IBAN: NL49TRIO0254729886<br>
+            </p>
+          </div>
+          <div class="col-md-3 text-light">
+            <h2>Blog</h2>
+          </div>
+          <div class="col-md-3 text-light">
+            <b-img
+              fluid="fluid"
+              blank-color="#777"
+              src="~/assets/images/Logo-Yep-300x122.png"
+              :srcset="require(`~/assets/images/Logo-Yep-300x122.png`).srcSet"
+            ></b-img>
+            <p>&nbsp;</p>  
+            Telefoon: 085-7606266
+          </div>
+
+        </div>  
+       
+      </div>
+    </div>  
   </div>
 </template>
 

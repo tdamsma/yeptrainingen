945169086045f6d3311a084b740cef848a26bc1a
Thijs Damsma <tdamsma@gmail.com>
2021-10-04 21:17:37 +0200

    Wijzigingen gijs september (#147)
    * Alle wijzigingen Gijs
    
    * linting
    
    * update readme
    
    * yarn update
    
    * update gh action
    
    * let yarn reinstall
    
    * typo
    
    Co-authored-by: Gijs <gijs@webfacet.nl>

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,5 @@
 <template lang="html">
   <div>
-    <div class="jumbotron jumbotron-fluid yep-geel">
-      <div class="container">
-        <h1 class="display-4">Benieuwd naar het digitale aanbod van Yep?</h1>
-        <p>
-          Doordat fysieke trainingen in deze tijd niet altijd wenselijk zijn,
-          <a href="https://yeptrainingen.nl/trainingsaanbod/">begeleiden wij je ook graag digitaal!</a>
-        </p>
-      </div>
-    </div>
     <div class="container-fluid container-left">
       <div class="row">
         <div class="container">
@@ -20,35 +11,56 @@
               :srcset="require(`~/assets/images/training-yep.jpg?sizes[]=233&amp;sizes[]=566`).srcSet"
             ></b-img>
           </div>
-          <div class="text-overlay yep-geel">
-            <h1 class="title">
-              <div style="text-decoration: underline">INZICHT, DURF EN VAARDIGHEID</div>
-              <div>VOOR MAATSCHAPPELIJKE IMPACT</div>
-            </h1>
-            <h2 class="subtitle">Trainings- en coachingsbureau voor gedreven professionals</h2>
+          <div class="text-overlay" style="background-color: #f2f2f2">
+            <h2>
+              <div>Inzicht, durf en vaardigheid</div>
+              <div style="color: #e8c902">voor maatschappelijke impact</div>
+            </h2>
           </div>
         </div>
       </div>
     </div>
     <div class="jumbotron jumbotron-fluid yep-geel">
       <div class="container">
-        <h2>De drang om te leren.</h2>
-        <h2>Wij zien het bij zo veel professionals.</h2>
-        <h2>En we weten hoe graag organisaties daar ruimte voor willen maken.</h2>
-        <p>
-          Wij zijn er voor professionals die staan te popelen om hun stem verder te ontdekken en te laten horen. Die een idee hebben over hoe het anders kan en
-          willen weten hoe ze dat binnen hun organisatie waar kunnen maken. Die soms nog aan zichzelf twijfelen en merken dat organisaties complex zijn. Maar
-          die zich razendsnel nieuwe vaardigheden eigen willen maken. Enerzijds om zelf te groeien, anderzijds omdat ze weten dat ze een tastbare bijdrage
-          kunnen leveren aan de organisatie en de samenleving als ze echt ergens voor durven staan.
-        </p>
+        Wij staan paraat voor professionals die geloven dat de maatschappij duurzamer en eerlijker kan zijn. En die de drang hebben om te leren. Samen met hun
+        organisaties ontwerpen en verzorgen we trainings- en coachingstrajecten. Zodat de professionals nog beter worden in wat ze goed kunnen: werken aan
+        vraagstukken die er toe doen.
       </div>
     </div>
-    <div class="jumbotron jumbotron-fluid">
+    <div class="jumbotron jumbotron-fluid" style="background-color: #585656">
       <div class="container">
         <div class="row justify-content-around">
-          <div class="col-md-3 border bg-light p-2 text-center"><h2>REVIEWS</h2></div>
-          <div class="col-md-3 border bg-light p-2 text-center"><h2>TOP PROGRAMMA</h2></div>
-          <div class="col-md-3 border bg-light p-2 text-center"><h2>BLOGS</h2>Hoe begeleid ik een hybride overleg?</div>
+          <div class="col-md-3 bg-light p-2 text-center">
+            <h2>Reviews</h2>
+            <div>
+              <div class="springest-review-widget">
+                <a href="//www.springest.nl/yep-trainingen-2#ervaringen"></a>
+              </div>
+              <p>
+                <script src="//www.springest.nl/reviewwidget.js" async="" />
+              </p>
+            </div>
+          </div>
+          <div class="col-md-3 border bg-light p-2 text-center">
+            <h2>Uitgelicht</h2>
+            <b-img
+              fluid="fluid"
+              blank-color="#777"
+              src="~/assets/images/uitgelicht1.png"
+              :srcset="require(`~/assets/images/uitgelicht1.png?sizes[]=200&amp;sizes[]=566`).srcSet"
+            ></b-img>
+            <a href="welke-rol-pak-jij-als-cursist" class="d-block mt-2">Talentontwikkel programma>></a>
+          </div>
+          <div class="col-md-3 border bg-light p-2 text-center">
+            <h2>Recente blog</h2>
+            <b-img
+              fluid="fluid"
+              blank-color="#777"
+              src="~/assets/images/omgaanmetstress.jpg"
+              :srcset="require(`~/content/blog/welke-rol-pak-jij-als-cursist-boodschappen.jpg?sizes[]=200&amp;sizes[]=566`).srcSet"
+            ></b-img>
+            <a href="welke-rol-pak-jij-als-cursist" class="d-block mt-2">Welke rol pak jij als cursist? >></a>
+          </div>
         </div>
       </div>
     </div>
@@ -56,7 +68,8 @@
       <div class="row">
         <div class="col-md full-width">
           <br /><br />
-          <h1 class="display-4">Neem je talenten serieus.</h1>
+          <h1 style="color: #e8c902">[Online] training coaching en advies</h1>
+          <h1>Neem je talent en maatschappelijke ambitie serieus.</h1>
           <br />
         </div>
         <div class="col-md">
@@ -71,26 +84,26 @@
         </div>
       </div>
     </div>
-    <div class="jumbotron jumbotron-fluid" style="background-color:#585656">
+    <div class="jumbotron jumbotron-fluid" style="background-color: #585656">
       <div class="container">
         <h1 class="text-center text-light">Opdrachtgevers</h1>
       </div>
     </div>
     <div class="container-fluid m-0">
-            <b-img
-              fluid="fluid"
-              blank-color="#777"
-              src="~/assets/images/site-foto-1-of-1-3.jpg"
-              :srcset="require(`~/assets/images/site-foto-1-of-1-3.jpg`).srcSet"
-            ></b-img> 
+      <b-img
+        fluid="fluid"
+        blank-color="#777"
+        src="~/assets/images/site-foto-1-of-1-3.jpg"
+        :srcset="require(`~/assets/images/site-foto-1-of-1-3.jpg`).srcSet"
+      ></b-img>
     </div>
-    <div class="jumbotron jumbotron-fluid" style="background-color:#585656">
+    <div class="jumbotron jumbotron-fluid" style="background-color: #585656">
       <div class="container text-light border-top border-bottom">
         <h1 class="text-center my-4">Yep is hét trainingsbureau voor gedreven professionals.</h1>
-         <div class="row justify-content-around my-5">
+        <div class="row justify-content-around my-5">
           <div class="col-md-3 p-2 text-center">
-            <h2>Krachtig</h2>
-            <p>In korte tijd zichtbaar effectief.</p>
+            <h2>Maatschappelijk</h2>
+            <p>Jouw impact is ons doel.</p>
           </div>
           <div class="col-md-3 p-2 text-center">
             <h2>Actief</h2>
@@ -98,28 +111,42 @@
           </div>
           <div class="col-md-3 text-center">
             <h2>Flexibel</h2>
-             <p>Jouw ontwikkelvraag staat centraal.</p>
+            <p>Jouw ontwikkelvraag staat centraal.</p>
           </div>
         </div>
-        <p class="text-center my-5">Voorbeelden van onze trainingen en case studies vind je hier. Op zoek naar coaching voor jezelf of je medewerkers? Lees hier verder>>.</p>
+        <p class="text-center my-5">
+          Voorbeelden van onze trainingen en case studies vind je hier. Op zoek naar coaching voor jezelf of je medewerkers? Lees hier verder>>.
+        </p>
       </div>
     </div>
     <div class="jumbotron jumbotron-fluid">
       <div class="container text-center">
         <h1>Bekijk ons aanbod</h1>
-        <div class="row justify-content-around mt-3">
-          <div class="col-md-4 border bg-light p-3 text-center">
+        <div class="row justify-content-around mt-5">
+          <div class="col-md-4 border bg-light p-2 text-center">
             <h2>Trainingen</h2>
-            <p>Lees meer >></p>
+            <b-img
+              fluid="fluid"
+              blank-color="#777"
+              src="~/assets/images/JUMP.jpg"
+              :srcset="require(`~/assets/images/JUMP.jpg?sizes[]=233&amp;sizes[]=566`).srcSet"
+            ></b-img>
+            <a href="trainingsaanbod" class="d-block mt-2">Lees meer >></a>
           </div>
-          <div class="col-md-4 border bg-light p-3 text-center">
+          <div class="col-md-4 border bg-light p-2 text-center">
             <h2>Coaching</h2>
-            <p>Lees meer >></p>
-            </div>
+            <b-img
+              fluid="fluid"
+              blank-color="#777"
+              src="~/assets/images/omgaanmetstress.jpg"
+              :srcset="require(`~/assets/images/omgaanmetstress.jpg?sizes[]=233&amp;sizes[]=566`).srcSet"
+            ></b-img>
+            <a href="coaching" class="d-block mt-2">Lees meer >></a>
+          </div>
         </div>
       </div>
     </div>
-    <div class="jumbotron jumbotron-fluid" style="background-color:#585656">
+    <div class="jumbotron jumbotron-fluid" style="background-color: #585656">
       <div class="container">
         <div class="row">
           <div class="col-md-3 text-light">
@@ -127,12 +154,13 @@
           </div>
           <div class="col-md-3 text-light">
             <h2 class="mb-4">Gegevens</h2>
-            <p>Yep Trainingen<br>
-               <br>
-               <br><br>
-               KvK: 58989463<br>
-               BTW: NL001795751B66<br>
-               IBAN: NL49TRIO0254729886<br>
+            <p>
+              Yep Trainingen<br />
+              <br />
+              <br /><br />
+              KvK: 58989463<br />
+              BTW: NL001795751B66<br />
+              IBAN: NL49TRIO0254729886<br />
             </p>
           </div>
           <div class="col-md-3 text-light">
@@ -145,14 +173,12 @@
               src="~/assets/images/Logo-Yep-300x122.png"
               :srcset="require(`~/assets/images/Logo-Yep-300x122.png`).srcSet"
             ></b-img>
-            <p>&nbsp;</p>  
+            <p>&nbsp;</p>
             Telefoon: 085-7606266
           </div>
-
-        </div>  
-       
+        </div>
       </div>
-    </div>  
+    </div>
   </div>
 </template>
 

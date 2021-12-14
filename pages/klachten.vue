52f43c0674ef45c9e8a73d382221a6072138b4ae
Thijs Damsma <tdamsma@gmail.com>
2021-12-14 20:33:09 +0100

    auto format

--- pages/klachten.vue
+++ pages/klachten.vue
@@ -1,28 +1,41 @@
 <template lang="html">
-<div>
-  <div class="jumbotron jumbotron-fluid yep-geel">
+  <div>
+    <div class="jumbotron jumbotron-fluid yep-geel">
       <div class="row justify-content-around">
         <h1>Klachten</h1>
       </div>
-  </div> 
-  <div class="container p-4">
-    <p><strong>Wat mag de klant van ons verwachten?</strong> </p>
-    <p>Wij verzorgen cursussen en trainingen  die in samenspraak zijn vormgegeven (maatwerk). U mag van ons verwachten dat de cursus raak aansluit op uw doelen en dat wij hierin bijsturen, mocht u gaande weg de doelen bijstellen of aangeven dat u iets anders bedoeld heeft. </p>
-    <p><strong>Wat verstaan wij onder een klacht?</strong></p>
-    <p>Met een klacht bedoelen wij een schriftelijke mededeling waarin te kennen wordt gegeven waarom de klager meent te kort te zijn gedaan.</p>
-    <p><strong>Waar moet de klacht worden ingediend? </strong></p>
-    <p>De directie van Yep Trainingen ,  of via info@yeptrainingen.nl </p>
-    <p><strong>Onze reactie </strong></p>
-    <p>Binnen 4 werkdagen wordt een schriftelijk bericht van ontvangst gestuurd. Binnen 3 weken na de ontvangstbevestiging delen wij u ons standpunt over de klacht mee. Slagen wij daar niet in, dan wordt dit tijdig meegedeeld. Daarbij wordt tevens vermeld binnen welke termijn ons standpunt dan wel mag worden verwacht.</p>
-    <p><strong>Vertrouwelijkheid en administratie</strong></p> 
-    <p>Alle aan ons toegestuurde stukken worden vertrouwelijk behandeld. Alle geuite klachten alsmede onze reactie daarop worden minstens 5 jaar in ons archief bewaard.</p>
-    <p><strong>De door ons voorgestelde oplossing</strong><p>
-    <p>Wij zien een klacht als mogelijkheid om een sterkere relatie met de klant op te bouwen. Daarom mag u een genereuze oplossing van ons verwachten.</p>
-    <p><strong>Beroepsmogelijkheid</strong></p>
-    <p>Slagen partijen er niet in tot een bevredigende oplossing te komen, dan wordt een advies gevraagd aan een onafhankelijke, ter zake deskundig zijnde derde. Diens stellingname is voor beide partijen bindend.  Deze persoon is Dhr. P.K. Huige, Spadinalaan 94 te Amsterdam.</p>  
+    </div>
+    <div class="container p-4">
+      <p><strong>Wat mag de klant van ons verwachten?</strong></p>
+      <p>
+        Wij verzorgen cursussen en trainingen die in samenspraak zijn vormgegeven (maatwerk). U mag van ons verwachten dat de cursus raak aansluit op uw doelen
+        en dat wij hierin bijsturen, mocht u gaande weg de doelen bijstellen of aangeven dat u iets anders bedoeld heeft.
+      </p>
+      <p><strong>Wat verstaan wij onder een klacht?</strong></p>
+      <p>Met een klacht bedoelen wij een schriftelijke mededeling waarin te kennen wordt gegeven waarom de klager meent te kort te zijn gedaan.</p>
+      <p><strong>Waar moet de klacht worden ingediend? </strong></p>
+      <p>De directie van Yep Trainingen ,  of via info@yeptrainingen.nl</p>
+      <p><strong>Onze reactie </strong></p>
+      <p>
+        Binnen 4 werkdagen wordt een schriftelijk bericht van ontvangst gestuurd. Binnen 3 weken na de ontvangstbevestiging delen wij u ons standpunt over de
+        klacht mee. Slagen wij daar niet in, dan wordt dit tijdig meegedeeld. Daarbij wordt tevens vermeld binnen welke termijn ons standpunt dan wel mag worden
+        verwacht.
+      </p>
+      <p><strong>Vertrouwelijkheid en administratie</strong></p>
+      <p>
+        Alle aan ons toegestuurde stukken worden vertrouwelijk behandeld. Alle geuite klachten alsmede onze reactie daarop worden minstens 5 jaar in ons archief
+        bewaard.
+      </p>
+      <p><strong>De door ons voorgestelde oplossing</strong></p>
+      <p></p>
+      <p>Wij zien een klacht als mogelijkheid om een sterkere relatie met de klant op te bouwen. Daarom mag u een genereuze oplossing van ons verwachten.</p>
+      <p><strong>Beroepsmogelijkheid</strong></p>
+      <p>
+        Slagen partijen er niet in tot een bevredigende oplossing te komen, dan wordt een advies gevraagd aan een onafhankelijke, ter zake deskundig zijnde
+        derde. Diens stellingname is voor beide partijen bindend. Deze persoon is Dhr. P.K. Huige, Spadinalaan 94 te Amsterdam.
+      </p>
+    </div>
   </div>
-
-</div>
 </template>
 
 <script>

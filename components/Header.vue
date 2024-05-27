3c3ae3e79ff22902a5b9207f417983547c5259c4
Thijs Damsma <thijs@damsma.net>
2024-05-27 23:21:24 +0200

    update content pages

--- components/Header.vue
+++ components/Header.vue
@@ -9,13 +9,31 @@ div
 
       b-collapse#nav-collapse(is-nav='')
         b-navbar-nav.ml-4
-          b-nav-item(to='/') Yep
-          b-nav-item(href='/team') Ons team
-          b-nav-item(href='/trainingen') Trainingen
-          b-nav-item(href='/coaching') Coaching
-          b-nav-item(href='/onze-aanpak') Onze aanpak
-          b-nav-item(to='/blog') Blog
+          b-nav-item(to='/') {{ $t('home') }}
+          b-nav-item(href='/team') {{ $t('team') }}
+          b-nav-item(href='/trainingen') {{ $t('trainings') }}
+          b-nav-item(href='/coaching') {{ $t('coaching') }}
+          b-nav-item(href='/onze-aanpak') {{ $t('ourApproach') }}
+          b-nav-item(to='/blog') {{ $t('blog') }}
+        b-navbar-nav.ml-auto
+          b-nav-item-dropdown(right)
+            template(v-slot:button-content)
+              span.flag-icon(:class="`flag-icon-${$i18n.locale}`")
+              | {{ $i18n.locale.toUpperCase() }}
+            b-dropdown-item(@click="changeLocale('en')") English
+            b-dropdown-item(@click="changeLocale('nl')") Nederlands
+
 </template>
+
+<script>
+export default {
+  methods: {
+    changeLocale(locale) {
+      this.$i18n.locale = locale;
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 @import 'assets/scss/custom.scss';

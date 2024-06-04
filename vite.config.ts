d760c3e982c593a3167928dfa6f1a063af976e37
Thijs Damsma <thijs@damsma.net>
2024-06-04 10:53:07 +0200

    initial setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import { paraglide } from '@inlang/paraglide-sveltekit/vite'
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
+	plugins: [paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),
 		enhancedImages(),
 		sveltekit()
 	],

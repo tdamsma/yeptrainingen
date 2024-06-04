95d71af8889d0129286f56a1906de188c96b1094
Thijs Damsma <thijs@damsma.net>
2024-06-04 10:53:07 +0200

    initial setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script>
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { i18n } from '$lib/i18n';
+
 	import { onMount } from 'svelte';
 	import 'bootstrap/dist/css/bootstrap.min.css';
 	import '$lib/scss/custom.scss';
@@ -14,68 +17,69 @@
 </script>
 
 <template lang="pug">
-  div
-    nav.navbar.navbar-expand-lg.navbar-light.bg-light
-      .container.d-flex.justify-content-between
-        a.navbar-brand(href="/")
-          enhanced:img.ml-2(src="/static/images/Logo-Yep-300x122.png", alt="yeptrainingen")
-        button.navbar-toggler.mr-2(type="button", data-toggle="collapse", data-target="#nav-collapse", aria-controls="nav-collapse", aria-expanded="false", aria-label="Toggle navigation")
-          span.navbar-toggler-icon
-        #nav-collapse.collapse.navbar-collapse
-          ul.navbar-nav.ml-4
-            li.nav-item
-              a.nav-link(href="/") Yep
-            li.nav-item
-              a.nav-link(href="/team") Ons team
-            li.nav-item
-              a.nav-link(href="/trainingen") Trainingen
-            li.nav-item
-              a.nav-link(href="/coaching") Coaching
-            li.nav-item
-              a.nav-link(href="/onze-aanpak") Onze aanpak
-            li.nav-item
-              a.nav-link(href="/blog") Blog
-    slot
-    .jumbotron.jumbotron-fluid(style="background-color: #807d7d")
-      .container
-        .row
-          .col-md-4.text-light
-            h2 Site-map
-            .row
-              .col-sm-6
-                | &gt;&nbsp; #[a.link-geel(href="/") Yep] #[br]
-                | &gt;&nbsp; #[a.link-geel(href="/team") Ons team] #[br]
-                | &gt;&nbsp; #[a.link-geel(href="/trainingen") Trainingen] #[br]
-                | &gt;&nbsp; #[a.link-geel(href="/coaching") Coaching] #[br]
-              .col-sm-6
-                | &gt;&nbsp; #[a.link-geel(href="/blog") Blog] #[br]
-                | &gt;&nbsp; #[a.link-geel(href="/tarieven") Tarieven] #[br]
-                | &gt;&nbsp; #[a.link-geel(href="/klachten") Klachten] #[br]
-                | &gt;&nbsp; #[a.link-geel(href="/contact") Contact] #[br]
-          .col-md-1
-          .col-md-3.text-light
-            h2.mb-4 Gegevens
-            p
-              | Yep Trainingen #[br]
-              |  #[br]
-              |  #[br] #[br]
-              | KvK: 58989463 #[br]
-              | BTW: NL001795751B66 #[br]
-              | IBAN: NL49TRIO0254729886 #[br]
-          .col-md-1
-          .col-md-3.text-light
-            enhanced:img(src="/static/images/Logo-Yep-300x122.png", alt="yeptrainingen")
-            p
-            a(href="mailto:info@yeptrainingen.nl", target="_blank")
-              enhanced:img(src="/static/images/email.png", alt="email")
-            a(href="https://www.facebook.com/Yeptrainingen", target="_blank")
-              enhanced:img(src="/static/images/facebook.png", alt="facebook")
-            a(href="https://twitter.com/yeptrainingen", target="_blank")
-              enhanced:img(src="/static/images/twitter.png", alt="twitter")
-            a(href="https://www.linkedin.com/company/marijn-blaisse-trainingen", target="_blank")
-              enhanced:img(src="/static/images/linkedin.png", alt="linkedin")
-            p
-            | Telefoon: 085-7606266
+  ParaglideJS( {i18n} )
+    div
+      nav.navbar.navbar-expand-lg.navbar-light.bg-light
+        .container.d-flex.justify-content-between
+          a.navbar-brand(href="/")
+            enhanced:img.ml-2(src="/static/images/Logo-Yep-300x122.png", alt="yeptrainingen")
+          button.navbar-toggler.mr-2(type="button", data-toggle="collapse", data-target="#nav-collapse", aria-controls="nav-collapse", aria-expanded="false", aria-label="Toggle navigation")
+            span.navbar-toggler-icon
+          #nav-collapse.collapse.navbar-collapse
+            ul.navbar-nav.ml-4
+              li.nav-item
+                a.nav-link(href="/") Yep
+              li.nav-item
+                a.nav-link(href="/team") Ons team
+              li.nav-item
+                a.nav-link(href="/trainingen") Trainingen
+              li.nav-item
+                a.nav-link(href="/coaching") Coaching
+              li.nav-item
+                a.nav-link(href="/onze-aanpak") Onze aanpak
+              li.nav-item
+                a.nav-link(href="/blog") Blog
+      slot
+      .jumbotron.jumbotron-fluid(style="background-color: #807d7d")
+        .container
+          .row
+            .col-md-4.text-light
+              h2 Site-map
+              .row
+                .col-sm-6
+                  | &gt;&nbsp; #[a.link-geel(href="/") Yep] #[br]
+                  | &gt;&nbsp; #[a.link-geel(href="/team") Ons team] #[br]
+                  | &gt;&nbsp; #[a.link-geel(href="/trainingen") Trainingen] #[br]
+                  | &gt;&nbsp; #[a.link-geel(href="/coaching") Coaching] #[br]
+                .col-sm-6
+                  | &gt;&nbsp; #[a.link-geel(href="/blog") Blog] #[br]
+                  | &gt;&nbsp; #[a.link-geel(href="/tarieven") Tarieven] #[br]
+                  | &gt;&nbsp; #[a.link-geel(href="/klachten") Klachten] #[br]
+                  | &gt;&nbsp; #[a.link-geel(href="/contact") Contact] #[br]
+            .col-md-1
+            .col-md-3.text-light
+              h2.mb-4 Gegevens
+              p
+                | Yep Trainingen #[br]
+                |  #[br]
+                |  #[br] #[br]
+                | KvK: 58989463 #[br]
+                | BTW: NL001795751B66 #[br]
+                | IBAN: NL49TRIO0254729886 #[br]
+            .col-md-1
+            .col-md-3.text-light
+              enhanced:img(src="/static/images/Logo-Yep-300x122.png", alt="yeptrainingen")
+              p
+              a(href="mailto:info@yeptrainingen.nl", target="_blank")
+                enhanced:img(src="/static/images/email.png", alt="email")
+              a(href="https://www.facebook.com/Yeptrainingen", target="_blank")
+                enhanced:img(src="/static/images/facebook.png", alt="facebook")
+              a(href="https://twitter.com/yeptrainingen", target="_blank")
+                enhanced:img(src="/static/images/twitter.png", alt="twitter")
+              a(href="https://www.linkedin.com/company/marijn-blaisse-trainingen", target="_blank")
+                enhanced:img(src="/static/images/linkedin.png", alt="linkedin")
+              p
+              | Telefoon: 085-7606266
 </template>
 
 <style lang="scss">
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
+import { paraglide } from '@inlang/paraglide-sveltekit/vite'
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit()],
+	plugins: [paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),
+		enhancedImages(),
+		sveltekit()
+	],
 	server: {
 		fs: {
 			allow: [

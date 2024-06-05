bb8e82251eca3d08d123b855513e1ed7d84ce34b
Thijs Damsma <thijs@damsma.net>
2024-06-05 10:24:37 +0200

    refine rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,11 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:svelte/recommended',
+		'plugin:jsx-a11y/recommended',
 		'prettier'
 	],
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
+	plugins: ['@typescript-eslint', 'jsx-a11y'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
@@ -25,7 +26,27 @@ module.exports = {
 			parser: 'svelte-eslint-parser',
 			parserOptions: {
 				parser: '@typescript-eslint/parser'
+			},
+			rules: {
+				'svelte/valid-compile': 'off',
+				'svelte/no-at-html-tags': 'warn',
+				'jsx-a11y/no-noninteractive-tabindex': 'warn',
+				'jsx-a11y/no-noninteractive-element-interactions': 'warn',
+				'css-unused-selector': 'off'
 			}
 		}
-	]
+	],
+	rules: {
+		'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+		'@typescript-eslint/ban-types': [
+			'error',
+			{
+				types: {
+					Function: false
+				},
+				extendDefaults: true
+			}
+		],
+		'@typescript-eslint/no-explicit-any': 'off'
+	}
 };

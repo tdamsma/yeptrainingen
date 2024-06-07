53a897048de386118e7620afa0073804a0613e1f
Thijs Damsma <thijs@damsma.net>
2024-06-07 23:31:10 +0200

    trainings pagina engels werkt

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,25 +1,36 @@
-const importPaths = {
+import type { AvailableLanguageTag } from '$lib/paraglide/runtime.js';
+const importPathsNl = {
     blog: import.meta.glob('$content/blog/*.nl.md'),
     coaching: import.meta.glob('$content/coaching/*.nl.md'),
     opdrachtgevers: import.meta.glob('$content/opdrachtgevers/*.nl.md'),
     team: import.meta.glob('$content/team/*.nl.md'),
     trainingen: import.meta.glob('$content/trainingen/*.nl.md')
 };
+const importPathsEn = {
+    blog: import.meta.glob('$content/blog/*.en.md'),
+    coaching: import.meta.glob('$content/coaching/*.en.md'),
+    opdrachtgevers: import.meta.glob('$content/opdrachtgevers/*.en.md'),
+    team: import.meta.glob('$content/team/*.en.md'),
+    trainingen: import.meta.glob('$content/trainingen/*.en.md')
+};
 
-const validCategories = ['blog', 'coaching', 'opdrachtgevers', 'team', 'trainingen'];
+const importPathsMap = {
+    nl: importPathsNl,
+    en: importPathsEn
+};
 
-export const fetchContent = async (category) => {
-	if (!validCategories.includes(category)) {
-		throw new Error('Invalid category');
-	}
 
-	const allPostFiles = importPaths[category];
-	const iterablePostFiles = Object.entries(allPostFiles);
+type ValidCategory = 'blog' | 'coaching' | 'opdrachtgevers' | 'team' | 'trainingen';
+
+export const fetchContent = async (category: ValidCategory, language: AvailableLanguageTag) => {
+    const importPaths = importPathsMap[language];
+    const allPostFiles = importPaths[category];
+    const iterablePostFiles = Object.entries(allPostFiles);
 
     const allPosts = await Promise.all(
         iterablePostFiles.map(async ([path, resolver]) => {
             const { metadata } = await resolver();
-            const postPath = path.replace(`/content/${category}/`, '').replace('.nl.md', '');
+            const postPath = path.replace(`/content/${category}/`, '').replace(`.${language}.md`, '');
             return {
                 meta: metadata,
                 path: postPath
@@ -27,5 +38,5 @@ export const fetchContent = async (category) => {
         })
     );
 
-	return allPosts;
+    return allPosts;
 };

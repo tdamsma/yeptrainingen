557454c5201188b7071b7ed6d350294d8023cf27
Thijs Damsma <thijs@damsma.net>
2024-06-08 08:05:10 +0200

    coaching pagina

--- src/routes/coaching/[coaching_title]/+page.ts
+++ src/routes/coaching/[coaching_title]/+page.ts
@@ -1,11 +1,19 @@
 import { fetchContent } from '$lib/utils';
+import { languageTag } from '$lib/paraglide/runtime.js';
 
-export async function load({ params }) {
-	const post = await import(`$content/coaching/${params.coaching_title}.md`);
+export async function load({ depends, params }) {
+	depends("paraglide:lang");
+
+	let post;
+	try {
+		post = await import(`$content/coaching/${params.coaching_title}.${languageTag()}.md`);
+	} catch (error) {
+		post = await import(`$content/coaching/_.${languageTag()}._.md`);
+	}
 	const { title, date, img } = post.metadata;
 	const content = post.default;
 
-	const allPosts = await fetchContent('coaching');
+	const allPosts = await fetchContent('coaching', languageTag());
 	const sortedPosts = allPosts.sort((a, b) => a.meta.volgnummer - b.meta.volgnummer);
 
 	const currentIndex = sortedPosts.findIndex((p) => p.path === params.coaching_title);
@@ -24,7 +32,7 @@ export async function load({ params }) {
 
 /** @type {import('./$types').EntryGenerator} */
 export async function entries() {
-	const allPosts = await fetchContent('coaching');
+	const allPosts = await fetchContent('coaching', languageTag());
 	return allPosts.map((post) => ({ coaching_title: post.path }));
 }
 

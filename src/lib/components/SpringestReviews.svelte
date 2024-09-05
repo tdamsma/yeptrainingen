b1218ff51d311fff3bacef88a45e644deb7cb19a
Thijs Damsma <thijs@damsma.net>
2024-09-05 23:43:25 +0200

    small improvements

--- src/lib/components/SpringestReviews.svelte
+++ src/lib/components/SpringestReviews.svelte
@@ -44,13 +44,13 @@
 	function closeModal() {
 		modalVisible = false;
 	}
-	function handleClickOutside(event) {
-		if (event.target.classList.contains('modal')) {
+	function handleClickOutside(event: MouseEvent) {
+		if (event.target && (event.target as HTMLElement).classList.contains('modal')) {
 			modalVisible = false;
 		}
 	}
 
-	function handleKeydown(event) {
+	function handleKeydown(event: KeyboardEvent) {
 		if (event.key === 'Escape') {
 			modalVisible = false;
 		}
@@ -139,9 +139,6 @@
 {/if}
 
 <style>
-	.list-item {
-		overflow: hidden;
-	}
 	.container {
 		height: 300px;
 		overflow: hidden;
